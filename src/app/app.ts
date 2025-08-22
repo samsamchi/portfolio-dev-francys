adb40fa94725eed6218f0db82caa97c505fb8600
Samuel Oliveira <samueloliveirasantos003@gmail.com>
2025-08-22 16:17:48 -0300

    feat: modelo 3D planeta terra cartoon, foto de perfil em navbar e outras estilizações

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { AvatarModule } from 'primeng/avatar';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AvatarModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class App {
   protected readonly title = signal('portfolio-dev-francys');
